405ec23c5aad68ed1cdbe56ce5d3dace61a12820
javi <javierromero.18@campuscamara.es>
2020-12-01 12:49:56 +0100

    Adición objetos y modificación variada en los JS

--- javascript/miel.js
+++ javascript/miel.js
@@ -16,18 +16,6 @@ this.fecMiel = function(fecha) {
     this.fecha = fecha;
 };
 
-var nombreMiel = function() {
-    return this.nombre;
-};
-
-this.infoMiel = function() {
-    return this.info;
-};
-
-this.fechaMiel = function() {
-    return this.fecha;
-};
-
 var miel1 = new miel("Miel de Romero", "La salvia es un arbusto aromático muy conocido por sus propiedades terapéuticas.", "15/11/2020");
 var miel2 = new miel("Miel de Tomillo", "La miel de Tomillo tiene una variedad de colores muy amplia dependiendo del tipo de tomillo al que pertenezca, van del blanco al rojo.", "20/11/2020");
 var miel3 = new miel("Miel de Encina", "La encina es el árbol nacional de España y se encuentra prácticamente en toda la península y de la que se extrae éste tipo de miel.", "26/11/2020");
--- javascript/pintura.js
+++ javascript/pintura.js
@@ -16,18 +16,6 @@ this.descPintura = function(descripcion) {
     this.descripcion = descripcion;
 };
 
-this.nombrePintura = function() {
-    return this.nombre;
-};
-
-this.colorPintura = function() {
-    return this.color;
-};
-
-this.descripcionPintura = function() {
-    return this.descripcion;
-};
-
 var pintura1 = new pintura("AISLASOL AL AGUA A17", "Mate", "Pintura al agua de alta cubrición para interiores, transpirable y sin olor.");
 var pintura2 = new pintura("ESTUCO VENECIANO TINTORETTO", "Satinado - Brillante", "Estuco de alta decoración para aplicación en interiores.");
 var pintura3 = new pintura("FONDO FIJADOR D13", "Incoloro", "Preparación para fijar y sellar superficies de albañilería, porosas o pulverulentas.");

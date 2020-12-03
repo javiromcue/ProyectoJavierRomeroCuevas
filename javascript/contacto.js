496b208aa6d0e236fb265ce708b0397fe6f2b258
javi <javierromero.18@campuscamara.es>
2020-12-03 18:47:41 +0100

    Edición contacto

--- javascript/contacto.js
+++ javascript/contacto.js
@@ -1,6 +1,7 @@
 window.onload = function() {
     document.getElementById("email").onblur = validacionEmail;
     document.getElementById("mensaje").onblur = validacionMensaje;
+    document.getElementById("telefono").onblur = validacionTelefono;
 
     function validacionEmail() {
         var email = document.getElementById("email").value;
@@ -14,6 +15,15 @@ window.onload = function() {
         var mensaje = document.getElementById("mensaje").value;
         if (mensaje.length < 20) {
             alert("Mensaje muy corto (> 20 letras).");
+            document.getElementById("mensaje").focus();
+        }
+    }
+
+    function validacionTelefono() {
+        var telefono = document.getElementById("telefono").value;
+        if (!(/^[9|6]{1}([\d]{2}[-]*){3}[\d]{2}$/.test(telefono))) {
+            alert("Teléfono erroneo.");
+            document.getElementById("telefono").focus();
         }
     }
 }

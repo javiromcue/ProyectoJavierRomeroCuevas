0871ee78017a2b511d0e58d6eb3508b3e57433b2
javi <javierromero.18@campuscamara.es>
2020-12-04 13:03:32 +0100

    Arrays añadidos

--- javascript/miel.js
+++ javascript/miel.js
@@ -7,3 +7,5 @@ function miel(nombre, info, fecha) {
 var miel1 = new miel("Miel de Romero", "La salvia es un arbusto aromático muy conocido por sus propiedades terapéuticas.", "15/11/2020");
 var miel2 = new miel("Miel de Tomillo", "La miel de Tomillo tiene una variedad de colores muy amplia dependiendo del tipo de tomillo al que pertenezca, van del blanco al rojo.", "20/11/2020");
 var miel3 = new miel("Miel de Encina", "La encina es el árbol nacional de España y se encuentra prácticamente en toda la península y de la que se extrae éste tipo de miel.", "26/11/2020");
+
+let arrMiel = [miel1, miel2, miel3]
--- javascript/pintura.js
+++ javascript/pintura.js
@@ -7,3 +7,5 @@ function pintura(nombre, color, descripcion) {
 var pintura1 = new pintura("AISLASOL AL AGUA A17", "Mate", "Pintura al agua de alta cubrición para interiores, transpirable y sin olor.");
 var pintura2 = new pintura("ESTUCO VENECIANO TINTORETTO", "Satinado - Brillante", "Estuco de alta decoración para aplicación en interiores.");
 var pintura3 = new pintura("FONDO FIJADOR D13", "Incoloro", "Preparación para fijar y sellar superficies de albañilería, porosas o pulverulentas.");
+
+let arrPintura = [pintura1, pintura2, pintura3]

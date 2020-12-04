903203184c256586e06f31156632564d09dbf764
javi <javierromero.18@campuscamara.es>
2020-12-04 11:17:37 +0100

    Edición JS

--- javascript/pintura.js
+++ javascript/pintura.js
@@ -4,18 +4,6 @@ function pintura(nombre, color, descripcion) {
     this.descripcion = descripcion;
 }
 
-this.nomPintura = function(nombre) {
-    this.nombre = nombre;
-};
-
-this.colPintura = function(color) {
-    this.color = color;
-};
-
-this.descPintura = function(descripcion) {
-    this.descripcion = descripcion;
-};
-
 var pintura1 = new pintura("AISLASOL AL AGUA A17", "Mate", "Pintura al agua de alta cubrición para interiores, transpirable y sin olor.");
 var pintura2 = new pintura("ESTUCO VENECIANO TINTORETTO", "Satinado - Brillante", "Estuco de alta decoración para aplicación en interiores.");
 var pintura3 = new pintura("FONDO FIJADOR D13", "Incoloro", "Preparación para fijar y sellar superficies de albañilería, porosas o pulverulentas.");
